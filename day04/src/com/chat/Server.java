68bf6701a2d10cf066806b928f24662bec1bb72d
sbkimdbr <sbkimdbr@gmail.com>
2020-10-31 00:48:21 +0900

    androidServer

--- day04/src/com/chat/Server.java
+++ day04/src/com/chat/Server.java
@@ -5,11 +5,14 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Set;
 
 import com.msg.Msg;
+
 
 public class Server {
 	
@@ -67,6 +70,7 @@ public class Server {
 	public void makeOut(Socket socket) throws IOException {
 		ObjectOutputStream oo;
 		oo = new ObjectOutputStream(socket.getOutputStream());
+		
 		maps.put(socket.getInetAddress().toString(), oo);
 		System.out.println("접속자수:"+maps.size());
 	}
@@ -87,10 +91,28 @@ public class Server {
 				try {
 					msg=(Msg)oi.readObject();
 					if(msg.getMsg().equals("q")) {
-					   
-						
 						throw new Exception();
 					    //강제로 예외 발생시켜서 아래의 catch구문으로 내려가도록 
+					}else if(msg.getMsg().equals("1")) {
+						String ip = 
+						socket.getInetAddress().toString();
+						ArrayList<String> ips= new ArrayList<>();
+						ips.add(ip);
+						msg.setIps(ips);
+						
+						//현재 사용자에 대한 정보 
+						Set<String> keys = maps.keySet();
+						//for 문으로 hashmap에 정보를넣어라 
+						HashMap<String,Msg>hm=
+								new HashMap<>();
+						for(String k:keys) {
+							hm.put(k,null);
+							
+						}
+						//1을 보낸 클라이언트 정보
+						//서버의 접속자 ip들이 hm에 들어있음 
+						msg.setMaps(hm);
+						
 					}
 					sendMsg(msg);//모든 클라이언트에게 메세지를 보내겠다
 					System.out.println("["+msg.getId()+"]"+msg.getMsg());
@@ -135,7 +157,8 @@ public class Server {
 			this.msg=msg;
 		}
 		
-	    //받은 메세지를 모든 사용자에게 보내줘야 한다. 
+	    //받은 메세지를 모든 사용자에게 보내줘야 한다.
+		//메세지를 보낼 때 msg라는 객체에 ip 가 있는지 없는지 확인  
 		@Override
 		public void run() {
 		Collection<ObjectOutputStream>cols=
@@ -143,17 +166,24 @@ public class Server {
 		Iterator<ObjectOutputStream> it =
 				cols.iterator();
 		while(it.hasNext()) {
+			//null과 ip 함께 체크 
 			try {
+				if(msg.getIps()!=null) {
+				for(String ip:msg.getIps()) {
+					maps.get(ip).writeObject(msg);
+				}
+					
+			    break; //특정 아이피에 보내고 끝낸
+				}
+			
 				it.next().writeObject(msg);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}//받은 메세지를 컬렉션으로빼고 이터레이터로 넣은 후 while루프로 값을 읽는
 		}
-		}
-		
-		
-	}
+	}	
+}
 	
 	
 	public static void main(String[] args) {
