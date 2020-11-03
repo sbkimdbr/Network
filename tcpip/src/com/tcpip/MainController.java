e94bbadb9739303e0a6d6e996c9ea7daa7513d18
sbkimdbr <sbkimdbr@gmail.com>
2020-11-04 00:37:50 +0900

    tcpip_springframwork

---
+++ tcpip/src/com/tcpip/MainController.java
@@ -0,0 +1,44 @@
+package com.tcpip;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.chat.Client;
+
+@Controller
+public class MainController {
+
+	Client client;
+	
+    public MainController() {
+	  client = new Client("192.168.123.107",5555,"[WEB]");
+	  try {
+		client.Connect();
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+   }
+	
+	@RequestMapping("/main.mc")
+	public ModelAndView main() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("main");
+		return mv;
+	}
+	@RequestMapping("/iot.mc")
+	public void iot() {
+		System.out.println("Iot send start...");
+		client.sendTarget("192.168.123.107", "100");
+		//PrinteWritter out = res.getWriter();
+		//out.print("ok");
+		//out.close();
+	}
+	@RequestMapping("/ftest.do")
+	public void phone() {
+		System.out.println("Phone send start...");
+	}
+	
+}
